3bd7bfb50628cae5bec92ba14b9c0a1385f3b30f
Matthieu Vachon <matt@streamingfast.io>
2023-02-06 16:31:09 -0500

    Updated some wrong copy/paste values

--- cmd/substreams-sink-kv/main.go
+++ cmd/substreams-sink-kv/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"runtime/debug"
 	"strings"
 	"time"
 
@@ -18,14 +19,8 @@ import (
 	"go.uber.org/zap"
 )
 
-// Commit sha1 value, injected via go build `ldflags` at build time
-var commit = ""
-
 // Version value, injected via go build `ldflags` at build time
 var version = "dev"
-
-// Date value, injected via go build `ldflags` at build time
-var date = ""
 
 var zlog, tracer = logging.RootLogger("sink-kv", "github.com/streamingfast/substreams-sink-kv/cmd/substreams-sink-kv")
 
@@ -76,19 +71,41 @@ func main() {
 
 func ConfigureVersion() CommandOption {
 	return CommandOptionFunc(func(cmd *cobra.Command) {
-		var labels []string
-		if len(commit) >= 7 {
-			labels = append(labels, fmt.Sprintf("Commit %s", commit[0:7]))
-		}
-
-		if date != "" {
-			labels = append(labels, fmt.Sprintf("Built %s", date))
-		}
-
-		cmd.Version = version
-
-		if len(labels) != 0 {
-			cmd.Version = fmt.Sprintf("%s (%s)", version, strings.Join(labels, ", "))
-		}
+		cmd.Version = versionString(version)
 	})
+}
+
+func versionString(version string) string {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		panic("we should have been able to retrieve info from 'runtime/debug#ReadBuildInfo'")
+	}
+
+	commit := findSetting("vcs.revision", info.Settings)
+	date := findSetting("vcs.time", info.Settings)
+
+	var labels []string
+	if len(commit) >= 7 {
+		labels = append(labels, fmt.Sprintf("Commit %s", commit[0:7]))
+	}
+
+	if date != "" {
+		labels = append(labels, fmt.Sprintf("Built %s", date))
+	}
+
+	if len(labels) == 0 {
+		return version
+	}
+
+	return fmt.Sprintf("%s (%s)", version, strings.Join(labels, ", "))
+}
+
+func findSetting(key string, settings []debug.BuildSetting) (value string) {
+	for _, setting := range settings {
+		if setting.Key == key {
+			return setting.Value
+		}
+	}
+
+	return ""
 }
