e7eee0bfdb1ff368524a160669650618e0f22fc3
Julien Cassis <juliencassis@gmail.com>
2023-03-16 14:39:16 -0400

    Add scan example

---
+++ examples/wasm-query-service/blockmeta_wasm_query/src/helper.rs
@@ -0,0 +1,17 @@
+
+
+pub fn parse_month(key: &String) -> String {
+    let index = key.len()-2;
+    return key[index..].to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::helper::parse_month;
+
+    #[test]
+    fn parse_month_test() {
+        let str = "month:first:201909".to_string();
+        assert_eq!("09" ,parse_month(&str));
+    }
+}
--- examples/wasm-query-service/blockmeta_wasm_query/src/lib.rs
+++ examples/wasm-query-service/blockmeta_wasm_query/src/lib.rs
@@ -1,12 +1,16 @@
+extern crate core;
+
 mod pb;
-use std::str;
+mod helper;
+
 #[allow(unused_imports)]
 use wasmedge_bindgen::*;
 use wasmedge_bindgen_macro::*;
-use crate::pb::service::{GetMonthRequest, MonthResponse, Month, Year, Block, Error};
+use crate::pb::service::{GetMonthRequest, MonthResponse, Month, Block, GetYearRequest, YearResponse};
 use crate::pb::blockmeta::{BlockMeta};
 use prost::Message;
 use substreams_sink_kv::prelude::*;
+
 
 #[wasmedge_bindgen]
 pub fn eth_service_v1_blockmeta_getmonth(v: Vec<u8>) -> Result<Vec<u8>, String> {
@@ -15,35 +19,58 @@ pub fn eth_service_v1_blockmeta_getmonth(v: Vec<u8>) -> Result<Vec<u8>, String> 
 
     let first_key = format!("month:first:{}{}", req.year,req.month);
     let last_key = format!("month:last:{}{}",req.year,req.month);
-    println!("first: {}", first_key);
-    println!("last_key: {}", last_key);
 
-    let optValues = store.get_many(vec![first_key.to_string(), last_key.to_string()]);
+    let kv_pairs_opt = store.get_many(vec![first_key.to_string(), last_key.to_string()]);
 
-    if optValues.is_none() {
-        let out = MonthResponse{
-            error: Some(Error{
-                code: 404,
-                message: format!("key: {}, {} not found",first_key, last_key).to_string(),
-            }),
-            month: None
-        };
-        return Ok(out.encode_to_vec())
+    match kv_pairs_opt {
+        Some(kv_pairs) => {
+            let out = MonthResponse{
+                month: Some(
+                    Month{
+                        year: req.year,
+                        month: req.month,
+                        first_block: value_to_block(kv_pairs.pairs[0].value.as_ref()),
+                        last_block: value_to_block(kv_pairs.pairs[1].value.as_ref()),
+                    }
+                )
+            };
+            Ok(out.encode_to_vec())
+        },
+        None => {
+            Err(format!("key: {}, {} not found",first_key, last_key))
+        }
     }
 
-    let values = optValues.unwrap();
+}
 
-    let out = MonthResponse{
-        error: None,
-        month: Some(
-            Month{
-                year: req.year,
-                month: req.month,
-                first_block: value_to_block(values.pairs[0].value.as_ref()),
-                last_block: value_to_block(values.pairs[1].value.as_ref()),
-            }
-        )
-    };
+#[wasmedge_bindgen]
+pub fn eth_service_v1_blockmeta_getyear(v: Vec<u8>) -> Result<Vec<u8>, String> {
+    let req = GetYearRequest::decode(&v[..]).expect("Failed to decode");
+    let store = Store::new();
+
+    let start = format!("month:first:{}01", req.year);
+    let end = format!("month:first:{}13", req.year);
+    let kv_pairs = store.scan(start, end, Some(12 as u32));
+
+    let mut out = YearResponse{ months: vec![] };
+
+    for kv_pair in kv_pairs.pairs {
+        out.months.push(Month{
+            year: req.year.to_string(),
+            month: helper::parse_month(&kv_pair.key),
+            first_block: value_to_block(&kv_pair.value),
+            last_block: None,
+        })
+    }
+
+    let start = format!("month:last:{}01", req.year);
+    let end = format!("month:last:{}13", req.year);
+    let kv_pairs = store.scan(start, end, Some(12 as u32));
+
+    for (i, kv_pair) in kv_pairs.pairs.iter().enumerate() {
+        out.months[i].last_block = value_to_block(&kv_pair.value);
+    }
+
     return Ok(out.encode_to_vec());
 }
 
