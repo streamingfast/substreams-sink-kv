415f67d02596ca6d9f6be1ef701f5b51e71d62c1
Matthieu Vachon <matt@streamingfast.io>
2023-05-05 14:58:20 -0400

    Custom image for goreleaser so properly build

---
+++ devel/docker/wasmedge/version.h
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2019-2022 Second State INC
+
+//===-- wasmedge/version.h - WasmEdge C API -------------------------------===//
+//
+// Part of the WasmEdge Project.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the version definitions of the WasmEdge C API.
+///
+//===----------------------------------------------------------------------===//
+
+#ifndef WASMEDGE_C_API_VERSION_H
+#define WASMEDGE_C_API_VERSION_H
+
+// WasmEdge version.
+#define WASMEDGE_VERSION "0.11.2"
+#define WASMEDGE_VERSION_MAJOR 0
+#define WASMEDGE_VERSION_MINOR 11
+#define WASMEDGE_VERSION_PATCH 2
+
+#define WasmEdge_Plugin_CurrentAPIVersion 2
+
+#endif // WASMEDGE_C_API_VERSION_H
