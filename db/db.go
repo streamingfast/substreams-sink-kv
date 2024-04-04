6ca06c8343484721bb35af91798d5b472a15723c
billettc <charles@cbillette.com>
2024-04-04 17:30:49 -0400

    prevent scanning all undos entry ...

--- db/db.go
+++ db/db.go
@@ -140,7 +140,7 @@ func (db *OperationDB) Flush(ctx context.Context, cursor *sink.Cursor) (count in
 func (db *OperationDB) PurgeUndoOperations(ctx context.Context, finalBlockHeight uint64) error {
 	keys := make([][]byte, 0)
 
-	scanOutput := db.store.Scan(ctx, undoKey(finalBlockHeight), undoKey(0), 0)
+	scanOutput := db.store.Scan(ctx, undoKey(finalBlockHeight), undoKey(finalBlockHeight-1000), 0)
 
 	if scanOutput.Err() != nil {
 		return fmt.Errorf("scanning undo operations for block %d: %w", finalBlockHeight, scanOutput.Err())
